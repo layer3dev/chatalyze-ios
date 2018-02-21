abad8db2e487eecd5c3279c089680e7f406e1a8f
gunjot <gunjot@mansainfotech.com>
2018-02-21 19:39:28 +0530

    create project

---
+++ Chatalyze/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Chatalyze
+//
+//  Created by Sumant Handa on 21/02/18.
+//  Copyright © 2018 Mansa Infotech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
