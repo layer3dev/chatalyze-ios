90ce8dcc1d614c8162cb9b307f5125e430ebb816
gunjot <gunjot@mansainfotech.com>
2018-05-09 16:14:25 +0530

    Added files in catalyse

---
+++ Chatalyze/config/headers/AppConfig/AppConnectioConfigForDev.swift
@@ -0,0 +1,30 @@
+//
+//  AppConnectionConfig.swift
+//  Chatalyze Autography
+//
+//  Created by Sumant Handa on 21/12/16.
+//  Copyright © 2016 Chatalyze. All rights reserved.
+//
+
+import Foundation
+
+class AppConnectionConfig {
+    
+    static let socketURL = "wss://peertopeer.chatalyze.com/oneonone"
+    //    static let socketURL = "wss://192.168.1.113:3001/oneonone"
+    //static let socketURL = "wss://192.168.1.17:3001/oneonone"
+    //static let webServiceURL = "https://chatalyze.com/api"
+    static let webServiceURL = "https://dev.chatalyze.com/api"
+    //static let webServiceURL = "https://beta.chatalyze.com/api"
+    //static let webServiceURL = "https://api.chatalyze.com/v1"
+    //static let webServiceURL = "http://192.168.1.113:8080/api"
+    //static let webServiceURL = "http://192.168.1.12:8080/api"
+    //static let userSocketURL = "https://chatalyze.com/"
+    static let userSocketURL = "https://dev.chatalyze.com/"
+    //static let userSocketURL = "https://beta.chatalyze.com/"
+    //static let userSocketURL = "https://api.chatalyze.com/"
+    //static let userSocketURL = "http://192.168.1.113:8080/"
+    //static let userSocketURL = "http://192.168.1.17:8080/"
+    
+}
+
---
+++ Chatalyze/config/headers/AppConfig/AppConnectionConfigForProduction.swift
@@ -0,0 +1,29 @@
+//
+//  AppConnectionConfig.swift
+//  Chatalyze Autography
+//
+//  Created by Sumant Handa on 21/12/16.
+//  Copyright © 2016 Chatalyze. All rights reserved.
+//
+
+import Foundation
+
+class AppConnectionConfigI {
+    
+    static let socketURL = "wss://peertopeer.chatalyze.com/oneonone"
+    //    static let socketURL = "wss://192.168.1.113:3001/oneonone"
+    //static let socketURL = "wss://192.168.1.17:3001/oneonone"
+    static let webServiceURL = "https://chatalyze.com/api"
+    //static let webServiceURL = "https://ui.chatalyze.com/api"
+    //static let webServiceURL = "https://beta.chatalyze.com/api"
+    //static let webServiceURL = "https://api.chatalyze.com/v1"
+    //static let webServiceURL = "http://192.168.1.113:8080/api"
+    //static let webServiceURL = "http://192.168.1.17:8080/api"    
+    static let userSocketURL = "https://chatalyze.com/"
+    //static let userSocketURL = "https://ui.chatalyze.com/"
+    //static let userSocketURL = "https://beta.chatalyze.com/"
+    //static let userSocketURL = "https://api.chatalyze.com/"
+    //static let userSocketURL = "http://192.168.1.113:8080/"
+    //static let userSocketURL = "http://192.168.1.17:8080/"
+    
+}
