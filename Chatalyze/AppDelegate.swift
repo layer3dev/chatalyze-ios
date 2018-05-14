2ed4e125c62c67cf2d9bba04d5b1628378ef8159
gunjot <gunjot@mansainfotech.com>
2018-05-14 17:47:44 +0530

    recompiled carthage

--- Chatalyze/AppDelegate.swift
+++ Chatalyze/AppDelegate.swift
@@ -22,6 +22,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         
         initialization()
+        let view = UIStackView()
         test()
         return true
     }
