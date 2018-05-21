c83e855721d2c98f8f0202dd2cbb8e9d71f9de99
gunjot <gunjot@mansainfotech.com>
2018-05-21 13:10:46 +0530

    removed carthage and pods from git cache

--- Pods/Target Support Files/Pods-ChatalyzeTests/Pods-ChatalyzeTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ChatalyzeTests : NSObject
-@end
-@implementation PodsDummy_Pods_ChatalyzeTests
-@end
--- Pods/Target Support Files/Pods-ChatalyzeUITests/Pods-ChatalyzeUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ChatalyzeUITests : NSObject
-@end
-@implementation PodsDummy_Pods_ChatalyzeUITests
-@end
