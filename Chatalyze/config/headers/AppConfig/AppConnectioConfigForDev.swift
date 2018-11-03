7b416fdcb3113d2c78cd16c151a9c0d819f839a5
gunjot <gunjot@mansainfotech.com>
2018-11-03 18:03:14 +0530

    removed check for signup

--- Chatalyze/config/headers/AppConfig/AppConnectioConfigForDev.swift
+++ Chatalyze/config/headers/AppConfig/AppConnectioConfigForDev.swift
@@ -13,15 +13,15 @@ class AppConnectionConfig {
     static let socketURL = "wss://peertopeer.chatalyze.com/oneonone"
     
     
-    static let webServiceURL = "http://192.168.1.35:3000/api"
-//    static let webServiceURL = "https://dev.chatalyze.com/api"
+//    static let webServiceURL = "http://192.168.1.35:3000/v1/"
+    static let webServiceURL = "https://dev.chatalyze.com/api"
     //static let webServiceURL = "https://beta.chatalyze.com/api"
     //static let webServiceURL = "https://api.chatalyze.com/v1"
     //static let webServiceURL = "http://192.168.1.113:8080/api"
     //static let webServiceURL = "http://192.168.1.12:8080/api"
     //static let userSocketURL = "https://chatalyze.com/"
-//    static let userSocketURL = "https://dev.chatalyze.com/"
-    static let userSocketURL = "http:///192.168.1.35:3000/"
+    static let userSocketURL = "https://dev.chatalyze.com/"
+//    static let userSocketURL = "http://192.168.1.35:3000/v1/"
     
     //static let userSocketURL = "https://beta.chatalyze.com/"
     //static let userSocketURL = "https://api.chatalyze.com/"
