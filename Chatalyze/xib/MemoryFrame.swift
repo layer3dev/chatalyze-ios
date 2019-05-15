875ac2dc9bb39d02094dec3945f2fe44e9d13aee
joban <jobanjit@mansainfotech.com>
2019-05-15 19:26:22 +0530

    resolved the checklist for the sticker

--- Chatalyze/xib/MemoryFrame.swift
+++ Chatalyze/xib/MemoryFrame.swift
@@ -18,16 +18,31 @@ class MemoryFrame:XibTemplate{
     @IBOutlet var memory:UILabel?
     @IBOutlet var userPic:UIImageView?
     var isPortraitInSize:Bool?
+    @IBOutlet var stickerView:UIView?
     
     @IBOutlet var widthOfStamp:NSLayoutConstraint?
     @IBOutlet var heightOfUserPic:NSLayoutConstraint?
     @IBOutlet var widthOfUserPic:NSLayoutConstraint?
     
+    @IBOutlet var stickerLeading:NSLayoutConstraint?
+    @IBOutlet var stickerBottom:NSLayoutConstraint?
+
     override func layoutSubviews() {
         super.layoutSubviews()
        
+        paintInterface()
         handleUI()
     }
+    
+    func paintInterface(){
+        
+        userPic?.layer.cornerRadius = 4
+        userPic?.layer.masksToBounds = true
+        
+        stickerView?.layer.cornerRadius = 4
+        stickerView?.layer.masksToBounds = true
+    }
+    
     
     func handleUI(){
         
@@ -45,19 +60,23 @@ class MemoryFrame:XibTemplate{
         
         heightOfUserPic?.constant = 63
         widthOfUserPic?.constant = 63
-        widthOfStamp?.constant = 275
+        widthOfStamp?.constant = 255
+        stickerLeading?.constant = 6
+        stickerBottom?.constant = 6
         name?.font = UIFont(name: "Nunito-Bold", size: 18)
-        date?.font = UIFont(name: "Nunito-Bold", size: 18)
-        memory?.font = UIFont(name: "Nunito-Regular", size: 15)
+        date?.font = UIFont(name: "Nunito-Bold", size: 15)
+        memory?.font = UIFont(name: "Nunito-Regular", size: 13)
     }
     
     func setUIForLandscape(){
         
-        heightOfUserPic?.constant = 45
-        widthOfUserPic?.constant = 45
-        widthOfStamp?.constant = 210
-        name?.font = UIFont(name: "Nunito-Bold", size: 12)
-        date?.font = UIFont(name: "Nunito-Bold", size: 12)
-        memory?.font = UIFont(name: "Nunito-Regular", size: 11)
+        heightOfUserPic?.constant = 36
+        widthOfUserPic?.constant = 36
+        widthOfStamp?.constant = 165
+        stickerLeading?.constant = 3
+        stickerBottom?.constant = 3
+        name?.font = UIFont(name: "Nunito-Bold", size: 11)
+        date?.font = UIFont(name: "Nunito-Bold", size: 9)
+        memory?.font = UIFont(name: "Nunito-Regular", size: 8)
     }
 }
