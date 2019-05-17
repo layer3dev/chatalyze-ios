40a256814162cb258e7f1536f92ce1874d71d31a
gunjot singh <gunjot@mansainfotech.com>
2019-05-17 14:05:20 +0530

    removed redundant identification

--- Chatalyze/network/FacebookLogin/FacebookLogin.swift
+++ Chatalyze/network/FacebookLogin/FacebookLogin.swift
@@ -161,7 +161,6 @@ class FacebookLogin{
         let token = rawInfo["token"].stringValue
         info.accessToken = token
         info.save()
-        SEGAnalytics.shared().identify(info.id, traits: ["name":info.firstName ?? "","email":info.email ?? ""])
         completion(true, "", info)
         return
     }
