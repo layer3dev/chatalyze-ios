47210a2a8d1e402dd825ec25cd7e95938bca8227
Mac mini ssd <macminissd@Macs-Mac-mini.local>
2019-09-04 11:40:37 +0530

    uploading the stable code

--- Chatalyze/config/configFlag/DevFlag.swift
+++ Chatalyze/config/configFlag/DevFlag.swift
@@ -8,8 +8,8 @@ import Foundation
 
 class DevFlag : NSObject{
 
-    @objc static let key = "processPoint"
-    @objc static let showAll = false
+    @objc static let key = "point"
+    @objc static let showAll = true
     @objc static let debug = true
 }
 
