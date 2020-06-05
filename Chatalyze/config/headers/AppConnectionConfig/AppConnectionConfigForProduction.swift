946aeb250ef57a45501556409eec9fd1c12d1c96
Yudhisther <yudhisther.aryan@mobileprogramming.com>
2020-06-06 02:13:15 +0530

    Fixed the few issues of the rotation

--- Chatalyze/config/headers/AppConnectionConfig/AppConnectionConfigForProduction.swift
+++ Chatalyze/config/headers/AppConnectionConfig/AppConnectionConfigForProduction.swift
@@ -19,4 +19,6 @@ class AppConnectionConfig {
     static let segmentIOKey = "IAY6kYifbTsfVhtqeRr4nPmC7jL3XGM2"
     
     static let bugsnagKey = "00c3c1e6099d88b1e00e275778167c33"
+    static let speedTestURL = "https://chatalyze.s3-us-west-2.amazonaws.com/reactions/logo_gmail_lockup_default_1x.png"
+
 }
