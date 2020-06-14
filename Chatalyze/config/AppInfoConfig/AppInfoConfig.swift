abd11696eec61a0ea87390156e8cfad45074cdbe
gunjot singh <gunjot@mansainfotech.com>
2020-06-14 17:34:52 +0530

    updated config

--- Chatalyze/config/AppInfoConfig/AppInfoConfig.swift
+++ Chatalyze/config/AppInfoConfig/AppInfoConfig.swift
@@ -9,10 +9,9 @@
 import Foundation
 
 class AppInfoConfig {
-    
     static var appName = "Chatalyze"
     static var deviceType = "ios"
-    static var appversion = "1.33"
+    static var appversion = "1.34"
     static var appType = "featured"
     //TODO:- Change the version
 }
