f342eb361770622a04dd0d147a1224acea79e1b1
Abhishek dhiman <abhishekdhiman@mansainfotech.com>
2021-04-01 15:49:18 +0530

    created storyBoard layour to claim ticket and added domain links to app

---
+++ Chatalyze/view/ClaimCustomTicketRootView/ClaimCustomTicketRootView.swift
@@ -0,0 +1,21 @@
+//
+//  ClaimCustomTicketRootView.swift
+//  Chatalyze
+//
+//  Created by Abhishek Dhiman on 01/04/21.
+//  Copyright © 2021 Mansa Infotech. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ClaimCustomTicketRootView : ExtendedView{
+    
+    var controller : ClaimCustomTicketController?
+    
+    override func viewDidLayout() {
+        super.viewDidLayout()
+    }
+    
+   
+}
